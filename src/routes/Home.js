9b61f534b78f3d79bb7e4ac00e679c797873ac97
grwang91 <003213@naver.com>
2020-11-26 19:55:28 +0900

    app completed

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,7 @@ import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
-    movies {
+    movies(limit: 40) {
       id
       medium_cover_image
     }
@@ -44,6 +44,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
   return (
@@ -53,9 +62,13 @@ export default () => {
         <Subtitle>GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading ...</Loading>}
-      {!loading &&
-        data.movies &&
-        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
     </Container>
   );
   if (loading) {
